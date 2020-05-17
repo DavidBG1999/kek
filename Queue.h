61b887680975d9ce9442f69b3beff448048e2d77
DavidBG1999 <43685883+DavidBG1999@users.noreply.github.com>
2020-05-17 21:28:53 +0300

    Add files via upload

---
+++ Queue.h
@@ -0,0 +1,33 @@
+#pragma once
+
+class Queue
+{
+private:
+	struct Node {
+		int value;
+		Node* next;
+		Node() {
+			next = nullptr;
+		}
+	};
+	Node* front;
+	Node* back;
+
+public:
+	Queue();
+	~Queue();
+	
+	typedef Node* iterater;
+	iterater getFront();
+	iterater getBack();
+	iterater getNext(iterater a);
+	int getValue(iterater a);
+	void push(int);
+	int pop();
+	bool is_empty();
+	void clear();
+	int peekFront();
+
+	int peekBack();
+
+};
